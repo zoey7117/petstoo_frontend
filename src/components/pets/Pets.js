677b37433e00180398cbd750873e0a420f384def
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-14 22:36:02 -0500

    cleaned up

--- src/components/pets/Pets.js
+++ src/components/pets/Pets.js
@@ -1,23 +1,66 @@
 import React from 'react'
-import PetCard from './PetCard'
-import './Pets.css'
-import { Card } from 'semantic-ui-react'
+import { Card, Image, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+class Pets extends React.Component{
+	constructor(props){
+    super()
+    this.state = {
+      front:true,
+      hungry:true,
+    }
+  }
+	toggleCard = ()=>{
+	  this.setState((prevState) =>{
+	    return {front:!prevState.front}
+	  })
+	}
+
+	hungryToggle =()=> {
+	  this.setState((prevState) => {
+	    return {hungry:!prevState.hungry}
+	  })
+	}
+
+	render(){
+		const adoptPet = this.props.adoptPet
+		const allpets = this.props.pets
+		console.log('props',this.props, 'state',this.state, allpets, adoptPet)
+
+		 return  allpets.map(pet => {
+	return(
+		<Card.Group centered>
+		    <Card className='pet-card' data-action='this-pet' data-id={pet.id} >
+		      <Image src={pet.image}  alt='' className='pet-image' />
+
+		      { (this.state.front) ?
+		      (<>
+		        <Card.Content >
+		          <Card.Description>
+			          <h3>hi! i'm {pet.name}</h3>
+			            <h5>i'm {pet.age} years old.</h5>
+			          <hr></hr>
+		          </Card.Description>
+
+								<Button size='mini' onClick={()=> adoptPet(pet.id)}>Adopt me!</Button>
+			          <Button size='mini' onClick={this.toggleCard}>flip to tend to me!</Button>
+	          </Card.Content>
 
 
-const Pets = props => {
-  const allpets = props.pets
-  console.log('allpets', allpets, 'props',props, props.adoptPet)
-  const adoptPet = <PetCard adoptPet={props.adoptPet}/>
-  const petCards = allpets.length > 0
-    ? allpets.map(pet => <PetCard img="img" scr={pet.image} pet={pet} key={pet.id}/>)
-    : []
-  // console.log(petCards, props)
-  return (<div>
-    <Card.Group>
-      {petCards}
+		        </>) :
+		        <Card.Content >
+		          { (this.state.hungry) ? <Button className='hungry' onClick={this.hungryToggle}>i'm stuffed, time for a nap!</Button> : <Button className='hungry'  onClick={this.hungryToggle}>i'm hungry, please feed me!</Button> }<br/><hr></hr>
+		              <Button size='mini' onClick={this.toggleCard}>flip back!</Button>
+		        </Card.Content >
+		      }
 
-    </Card.Group>
-  </div>)
+		    </Card >
+	  </Card.Group>
+	)
+})
 }
+}
+
+
 
 export default Pets
