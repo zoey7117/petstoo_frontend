041d2ef873b4d7549b46b30c66e4ef7053c31b0e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-17 15:28:22 -0500

    navbar styled

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,47 +1,64 @@
 import React from 'react'
-import { Grid, Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
-
+import {Grid, Menu} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import './NavBar.css'
+import ToggleButton from '../mobileNavbar/ToggleButton'
 
 class Navbar extends React.Component {
 
+  render() {
+
+    return (<nav id="navbar">
+      <Link to="/" className='navbar-logo'>Adopt a Pet!</Link>
+      <div className='spacer'/>
+      <div className='navbar-words'>
 
 
-	render(){
-	
-		return (
-			<Grid.Row>
-				<Grid.Column width={16}>
-					<Menu>
-						<Link className="item" to="/home">
-							Home
-						</Link>
-						{this.props.currentUser
-							?
-							<Menu.Menu position="right">
-								<Link  className="item" to={`/user`}>
-									{this.props.currentUser.name}
+          {
+            this.props.currentUser
+              ? <div>
+							<ul>
+							<li>
+								<Link to="/home">
+									Home
+								</Link>
+							</li>
+                  <li>
+                    <Link to={`/user`}>
+                      {this.props.currentUser.name}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.props.logOut}>
+                      Log out
+                    </Link>
+                  </li>
+								</ul>
+                </div>
 
+              : <div>
+								<ul>
+							<li>
+								<Link to="/home">
+									Home
 								</Link>
-								<Menu.Item onClick={this.props.logOut}>
-									Log out
-								</Menu.Item>
-							</Menu.Menu>
-							:
-							<Menu.Menu position="right">
-								<Link className="item" to="/login">
-									Login
-								</Link>
-								<Link className="item" to="/signup">
-									Sign Up
-								</Link>
-							</Menu.Menu>
-						}
-					</Menu>
-				</Grid.Column>
-			</Grid.Row>
-		)
-	}
+							</li>
+                  <li>
+                    <Link to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">
+                      Sign Up
+                    </Link>
+                  </li>
+								</ul>
+                </div>
+          }
+      </div>
+    </nav>)
+  }
 }
 
 export default Navbar
