cc0db2cb91cbe60a998308c4c9bbc47812dded0c
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-10 22:08:49 -0400

    added new pets

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,66 +1,55 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './NavBar.css'
-import ToggleButton from '../mobileNavbar/ToggleButton'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+import ToggleButton from '../mobileNavbar/ToggleButton';
 
 class Navbar extends React.Component {
-
-  render() {
-
-    return (<nav id="navbar">
-      <Link to="/home" className='navbar-logo'>Adopt a Pet</Link>
-      <div className='spacer'/>
-      <div className='navbar-words'>
-
-
-          {
-            this.props.currentUser
-              ? <div>
+	render() {
+		return (
+			<nav id="navbar">
+				<Link to="/home" className="navbar-logo">
+					Adopt a Pet
+				</Link>
+				<div className="spacer" />
+				<div className="navbar-words">
+					{this.props.currentUser ? (
+						<div>
 							<ul>
-							<li>
-								<Link to="/home">
-									Home
-								</Link>
-							</li>
-                  <li>
-                    <Link to={`/user`}>
-                      {this.props.currentUser.name}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={'/logout'} onClick={this.props.logOut}>
-                      Log out
-                    </Link>
-                  </li>
-								</ul>
-                </div>
-
-              : <div>
-								<ul>
-							<li>
-								<Link to="/home">
-									Home
-								</Link>
-							</li>
-                  <li>
-                    <Link to="/login">
-                      Login
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/signup">
-                      Sign Up
-                    </Link>
-                  </li>
-								</ul>
-                </div>
-          }
-      </div>
-			<div className='mobile-navbar-button'>
-	<ToggleButton click={this.props.buttonClickHandler}/>
-	</div>
-    </nav>)
-  }
+								<li>
+									<Link to="/home">Home</Link>
+								</li>
+								<li>
+									<Link to={`/user`}>{this.props.currentUser.name}</Link>
+								</li>
+								<li>
+									<Link to={'/logout'} onClick={this.props.logOut}>
+										Log out
+									</Link>
+								</li>
+							</ul>
+						</div>
+					) : (
+						<div>
+							<ul>
+								<li>
+									<Link to="/home">Home</Link>
+								</li>
+								<li>
+									<Link to="/login">Login</Link>
+								</li>
+								<li>
+									<Link to="/signup">Sign Up</Link>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+				<div className="mobile-navbar-button">
+					<ToggleButton click={this.props.buttonClickHandler} />
+				</div>
+			</nav>
+		);
+	}
 }
 
-export default Navbar
+export default Navbar;
