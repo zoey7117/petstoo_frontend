96df1d17602cef2f4fb052b1ed5e24a8214c2757
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-09-07 12:55:17 -0400

    back to original

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 ReactDOM.render(
-  <Router>
-    <Route path="/" component={App} />
-  </Router>, document.getElementById('root'));
+	<Router>
+		<ScrollToTop>
+			<Route path="/" component={App} />
+		</ScrollToTop>
+	</Router>,
+	document.getElementById('root')
+);
 
-
-  // ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
